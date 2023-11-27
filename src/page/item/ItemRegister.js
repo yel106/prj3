a43899badb6dce8949e6b063a2477dea1a3ba642
jeon602 <731aa@naver.com>
2023-11-27 14:26:00 +0900

    상품 등록 페이지 작성 중,,,,,

--- src/page/item/ItemRegister.js
+++ src/page/item/ItemRegister.js
@@ -2,6 +2,7 @@
 import {useState} from "react";
 import {useToast} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 export function ItemRegister() {
   const [id, setId] = useState("");
@@ -11,9 +12,23 @@ export function ItemRegister() {
   const [price, setPrice] = useState("");
   const [agency, setAgency] = useState("");
   const [releaseDate, setReleaseDate] = useState("");
-  const toast= useToast();
+  const toast = useToast();
   const navigate = useNavigate();
-  return(
+
+  function handleSubmit() {
+    axios
+      .post("api/item/register", {
+        id,
+        title,
+        artist,
+        releaseDate,
+        albumFormat,
+        agency,
+        price
+      })
+  }
+
+  return (
     <></>
   );
 }
