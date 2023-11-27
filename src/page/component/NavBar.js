5795cac8ee2b780cb6bb9de9841a0ae9f6fc6b97
smil98 <seungmilee98@gmail.com>
2023-11-27 17:32:45 +0900

    Nav Bar에 로그인 로그아웃 버튼 생성, handleLogout 함수 생성

--- src/page/component/NavBar.js
+++ src/page/component/NavBar.js
@@ -8,6 +8,26 @@ export function NavBar(props) {
   const navigate = useNavigate();
   const urlParams = new URLSearchParams();
   const location = useLocation();
+
+  function handleLogout() {
+    axios
+      .post("/api/member/logout")
+      .then(() => {
+        toast({
+          description: "성공적으로 로그아웃 되었습니다",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        toast({
+          description: "로그아웃 도중 에러가 발생했습니다",
+          status: "error",
+        });
+      })
+      .finally(() => console.log("useContext 관련 무언가 삽입해야함"));
+  }
+
   return (
     <Flex>
       <Button
@@ -36,6 +56,22 @@ export function NavBar(props) {
       >
         <FontAwesomeIcon icon={faUsers} />
         회원목록
+      </Button>
+      <Button
+        variant="ghost"
+        _hover={{ bg: "none" }}
+        onClick={() => navigate("/login")}
+        leftIcon={<FontAwesomeIcon icon={faRightToBracket} />}
+      >
+        로그인
+      </Button>
+      <Button
+        variant="ghost"
+        _hover={{ bg: "none" }}
+        onClick={handleLogout}
+        leftIcon={<FontAwesomeIcon icon={faRightFromBracket} />}
+      >
+        로그아웃
       </Button>
     </Flex>
   );
