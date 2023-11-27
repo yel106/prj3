ffbec1be01128da981f40f3458d9bd325ec896ac
kkssii0210 <kkssii0210@naver.com>
2023-11-27 14:00:28 +0900

    멤버 검색기능 추가

--- src/page/member/MemberList.js
+++ src/page/member/MemberList.js
@@ -3,7 +3,9 @@ import {
   Button,
   ButtonGroup,
   Center,
-  list,
+  Flex,
+  Input,
+  Select,
   Spinner,
   Table,
   Tbody,
@@ -13,23 +15,64 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+function SearchComponent() {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+  const [category, setCategory] = useState("all");
+  function handleSearch() {
+    const params = new URLSearchParams();
+    params.set("k", keyword);
+    params.set("c", category);
+    navigate("/member/list?" + params);
+  }
+  return (
+    <Center mt={5}>
+      <Flex gap={1}>
+        <Box>
+          <Select
+            defaultValue="all"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="all">이름검색</option>
+            <option value="logId">아이디검색</option>
+          </Select>
+        </Box>
+        <Box>
+          <Input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+        </Box>
+        <Button onClick={handleSearch}>
+          <FontAwesomeIcon icon={faSearch} />
+        </Button>
+      </Flex>
+    </Center>
+  );
+}
 
 export function MemberList() {
   const [list, setList] = useState(null);
   const navigate = useNavigate();
+  const location = useLocation(); // 현재 URL의 위치 정보를 가져옵니다.
   const [currentPage, setCurrentPage] = useState(0); // 현재 페이지 번호 (0부터 시작)
   const [totalPages, setTotalPages] = useState(0); // 총 페이지 수
-  const itemsPerPage = 20; // 페이지당 항목 수
+  const itemsPerPage = 10; // 페이지당 항목 수
   useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const keyword = queryParams.get("k");
+    const category = queryParams.get("c");
     axios
-      .get(`/member/list?page=${currentPage}&size=${itemsPerPage}`)
+      .get(
+        `/member/list?page=${currentPage}&size=${itemsPerPage}&k=${keyword}&c=${category}`,
+      )
       .then((response) => {
         setList(response.data.content);
         setTotalPages(response.data.totalPages);
       });
-  }, [currentPage]);
+  }, [currentPage, location.search]);
   if (list === null) {
     return <Spinner />;
   }
@@ -93,6 +136,9 @@ export function MemberList() {
           ))}
         </Tbody>
       </Table>
+      <Center>
+        <SearchComponent />
+      </Center>
       <Center>
         <ButtonGroup>
           <Button onClick={handlePreviousPage} disabled={currentPage === 0}>
