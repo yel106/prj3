c200dd90a59cb2d810b48c021f0d90a3464cd99b
smil98 <seungmilee98@gmail.com>
2023-11-29 12:10:39 +0900

    로그인창에 소셜 로그인 연동 버튼(카카오, 네이버, 구글) 생성, 핸들러 메소드 생성, Roboto 폰트 설치 및 적용

--- src/page/member/MemberLogin.js
+++ src/page/member/MemberLogin.js
@@ -1,7 +1,22 @@
-import {useState} from "react";
-import {Box, Button, FormControl, FormLabel, Heading, Input, useToast} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  IconButton,
+  Img,
+  Input,
+  useToast, VStack
+} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import {faGoogle} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faComment, faN} from "@fortawesome/free-solid-svg-icons";
 
 export function MemberLogin() {
   const [id, setId] = useState("");
@@ -27,10 +42,22 @@ export function MemberLogin() {
       });
   }
 
+  function handleKakaoLogin() {
+    console.log("카카오 로그인");
+  }
+
+  function handleNaverLogin() {
+    console.log("네이버 로그인");
+  }
+
+  function handleGoogleLogin() {
+    console.log("구글 로그인");
+  }
+
   return (
-    <Box>
-      <Heading textAlign="center">Login</Heading>
-      <FormControl>
+    <Box p={10} borderRadius={10} border="1px solid gray">
+      <Heading textAlign="center" mb={10}>Login</Heading>
+      <FormControl mb={5}>
         <FormLabel>아이디</FormLabel>
         <Input
           type="text"
@@ -39,7 +66,7 @@ export function MemberLogin() {
           onChange={(e) => setId(e.target.value)}
         />
       </FormControl>
-      <FormControl>
+      <FormControl mb={5}>
         <FormLabel>비밀번호</FormLabel>
         <Input
           type="password"
@@ -49,6 +76,27 @@ export function MemberLogin() {
         />
       </FormControl>
       <Button colorScheme="blue" onClick={handleLogin}>로그인</Button>
+
+      <VStack spacing={3}>
+        <Button style={{
+          backgroundImage:"url('https://study999888777.s3.ap-northeast-2.amazonaws.com/kakao_login_medium_narrow.png')",
+          width:"183px",
+          height:"45px"}}
+          onClick={handleKakaoLogin}
+        />
+        <Button style={{backgroundImage:"url('https://study999888777.s3.ap-northeast-2.amazonaws.com/btnG_%EC%99%84%EC%84%B1%ED%98%95.png')",
+          width:"183px",
+          height:"45px",
+          backgroundSize:"183px 45px"}}
+          onClick={handleNaverLogin}
+        />
+        <Button backgroundColor="#FFF" boxShadow="md" w="183px" h="45px" _hover={"none"}
+                leftIcon={<Img objectFit="cover" boxSize="35px" mr={2}
+                  src="https://study999888777.s3.ap-northeast-2.amazonaws.com/web_light_rd_na%401x.png"/>}
+                onClick={handleGoogleLogin} fontFamily="Roboto" fontWeight="500">
+          Google
+        </Button>
+      </VStack>
     </Box>
   )
 
