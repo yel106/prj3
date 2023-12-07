80f10fd6074de5271b0104b89368d5b91f2d03e9
jeon602 <731aa@naver.com>
2023-12-07 12:25:00 +0900

    BoardEdit 상품 편집 페이지 발매일자 수정 폼 오류수정

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -1,4 +1,17 @@
-import {Box, Button, Container, FormControl, FormLabel, Image, Input, Spinner, Stack, useToast} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Image,
+  Input,
+  Spinner,
+  Stack,
+  Switch,
+  Text,
+  useToast
+} from "@chakra-ui/react";
 import React, {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {useImmer} from "use-immer";
@@ -7,14 +20,17 @@ import axios from "axios";
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null); //객체 사용해서 가변적으로 상태 변경
   // /edit/:id
-  const { id } = useParams();
+  const {id} = useParams();
   const [fileURL, setFileURL] = useState('');
-  const [previousFileUrl, setPreviousFileUrl] = useState('');
-  const handleFileUrl=()=>{setPreviousFileUrl(fileURL);};
+  // const [previousFileUrl, setPreviousFileUrl] = useState('');
+  // const handleFileUrl = () => {
+  //   setPreviousFileUrl(fileURL);
+  // };
   //먼저 조회함.  updateBoard로 응답 받아옴
   const navigate = useNavigate();
   const toast = useToast();
   const [boardFiles, setBoardFiles] = useState(null);
+  const [removeFileIds, setRemoveFileIds] = useState([]);
 
 
   //
@@ -31,16 +47,17 @@ export function BoardEdit() {
   //     .finally(()=>console.log("얍"));
   // }, []);
 
-  useEffect(() => {
-    if (board !== null) {
-      setPreviousFileUrl(fileURL); // 이전 이미지 URL 설정
-    }
-  }, [board]);
+  // useEffect(() => {
+  //   if (board !== null) {
+  //     setPreviousFileUrl(fileURL); // 이전 이미지 URL 설정
+  //   }
+  // }, [board]);
 
   if (board === null) {
-    return <Spinner />;
+    return <Spinner/>;
   }
-  function handleFileUrlChange(e){
+
+  function handleFileUrlChange(e) {
     setFileURL(e.target.value);
   }
 
@@ -51,30 +68,33 @@ export function BoardEdit() {
       draft.title = e.target.value;
     });
   }
+
   //가수 명 수정
-  function handleArtistEdit(e){
-    updateBoard((draft)=>{
+  function handleArtistEdit(e) {
+    updateBoard((draft) => {
       draft.artist = e.target.value;
     });
   }
+
   //발매일자 수정
-  function handleReleaseDateEdit(e){
-    updateBoard((draft)=> {
+  function handleReleaseDateEdit(e) {
+    updateBoard((draft) => {
       draft.releaseDate = e.target.value;
     });
   }
-//가격 수정
+
+  //가격 수정
   function handlePriceEdit(e) {
     updateBoard((draft) => {
       draft.price = e.target.value;
     });
   }
 
-    function handleContentEdit(e) {
-      updateBoard((draft) => {
-        draft.content = e.target.value;
-      });
-    }
+  function handleContentEdit(e) {
+    updateBoard((draft) => {
+      draft.content = e.target.value;
+    });
+  }
 
 
   // 이미지 수정 코드
@@ -98,11 +118,12 @@ export function BoardEdit() {
     axios
       .put("/api/board/edit/" + id, {
         title: board.title,
-        artist:board.artist,
+        artist: board.artist,
         price: board.price,
-        content:board.content,
-        fileURL:board.fileURL,
-        releaseDate:board.releaseDate,//이미지도 전송
+        content: board.content,
+        fileURL: board.fileURL,
+        releaseDate: board.releaseDate,
+        removeFileIds,//이미지도 전송
       })
       .then((response) =>
         toast({
@@ -119,6 +140,16 @@ export function BoardEdit() {
   }
 
 
+  function handleRemoveFileSwitch(e) {
+    if (e.target.checked) {
+      setRemoveFileIds([...removeFileIds, e.target.value]);
+      updateBoard(draft => {
+        draft.boardFiles = draft.boardFiles.filter(file => file.id !== e.target.value);
+      });
+    } else {
+      setRemoveFileIds(removeFileIds.filter((item) => item !== e.target.value));
+    }
+  }
 
   //--------------------------------------등록된 상품 수정 폼
   return (
@@ -129,72 +160,85 @@ export function BoardEdit() {
       <FormControl>
         <FormLabel>Image</FormLabel>
         {/*처음 등록 했던 상품의 이미지 : 건들이지 않기.....젭알 */}
-        {board.boardFiles.map(file => <Box key={file.id}>
-          <Image src={file.fileUrl} alt={file.fileName} w="100%"/>
-        </Box>)}
-          {/*<Image key={fileURL} src={board.boardFiles.fileURL} border="1px solid red"/>*/}
-      </FormControl>
-      {/*앨범 타이틀. 가수명, 가격, 발매일자 ,발매 회사 순*/}
+        {board.boardFiles !== null && board.boardFiles.map(file => <Box key={file.id}>
+            <Image src={file.fileUrl} alt={file.fileName} w="100%"/>
+          <Text>등록되어 있는 이미지를 삭제하고 다시 등록 하시겠습니까?</Text>
+          <Switch
+            value={file.id}
+                    colorScheme="pink"
+                    onChange={handleRemoveFileSwitch}
+            />
+          </Box>
+        )}
+        </FormControl>
+        {/*앨범 타이틀. 가수명, 가격, 발매일자 ,발매 회사 순*/}
 
-      {/*타이틀 수정 폼*/}
-      <FormControl>
-        <FormLabel>Album Title</FormLabel>
-        <Input value={board.title} onChange={handleTitleEdit}/>
-      </FormControl>
-      {/*가수명 편집 */}
-      <FormControl>
-        <FormLabel>Artist Name Edit</FormLabel>
-        <Input value={board.artist} onChange={handleArtistEdit}/>
-      </FormControl>
-      {/*발매일자 편집 */}
-      <FormControl>
-        <FormLabel>ReleseDate Edit</FormLabel>
-        <Input
-          type="date"
-          value={board.releaseDate} onChange={handleReleaseDateEdit}/>
-      </FormControl>
-      {/*가격 수정 */}
-      <FormControl>
-        <FormLabel>Album Price Edit</FormLabel>
-        <Input value={board.price} onChange={handlePriceEdit}/>
-      </FormControl>
-      {/*앨범 상세 설명 란*/}
-      <FormControl>
-        <FormLabel>앨범 상품 상세 설명 수정</FormLabel>
-        <Input value={board.content} onChange={handleContentEdit}/>
-      </FormControl>
+        {/*타이틀 수정 폼*/}
+          <FormControl>
+          <FormLabel>Album Title</FormLabel>
+          <Input value={board.title} onChange={handleTitleEdit}/>
+    </FormControl>
+  {/*가수명 편집 */
+  }
+  <FormControl>
+    <FormLabel>Artist Name Edit</FormLabel>
+    <Input value={board.artist} onChange={handleArtistEdit}/>
+  </FormControl>
+  {/*발매일자 편집 */
+  }
+  <FormControl>
+    <FormLabel>ReleseDate Edit</FormLabel>
+    <Input
+      type="date"
+      value={board.releaseDate} onChange={handleReleaseDateEdit}/>
+  </FormControl>
+  {/*가격 수정 */
+  }
+  <FormControl>
+    <FormLabel>Album Price Edit</FormLabel>
+    <Input value={board.price} onChange={handlePriceEdit}/>
+  </FormControl>
+  {/*앨범 상세 설명 란*/
+  }
+  <FormControl>
+    <FormLabel>앨범 상품 상세 설명 수정</FormLabel>
+    <Input value={board.content} onChange={handleContentEdit}/>
+  </FormControl>
 
-      {/*----------------이미지 파일 수정 코드 --------------*/}
-      <FormControl>
-        <FormLabel>Album Image Update</FormLabel>
-        {/*<Text>Previous Image URL: {board.fileName}</Text>*/}
-        <Image
-          src={fileURL}
-          borderRadius="l"
-          border="0px solid black"
-        />
-        <Input
-          type="file"
-          accept="image/*"
-          multiple
+  {/*----------------이미지 파일 수정 코드 --------------*/
+  }
+  <FormControl>
+    <FormLabel>Album Image Update</FormLabel>
+    {/*<Text>Previous Image URL: {board.fileName}</Text>*/}
+    <Image
+      src={fileURL}
+      borderRadius="l"
+      border="0px solid black"
+    />
+    <Input
+      type="file"
+      accept="image/*"
+      multiple
 
-        />
-        {/*<FormHelperText color="red.200">*/}
-        {/*  한 개 파일은 1MB 이내, 총 용량은 10MB 이내로 첨부 가능합니다.*/}
-        {/*</FormHelperText>*/}
-      {/*|이미지 편집 이벤트가 없음,.*/}
-      </FormControl>
+    />
+    {/*<FormHelperText color="red.200">*/}
+    {/*  한 개 파일은 1MB 이내, 총 용량은 10MB 이내로 첨부 가능합니다.*/}
+    {/*</FormHelperText>*/}
+    {/*|이미지 편집 이벤트가 없음,.*/}
+  </FormControl>
 
 
-      <Stack>
-      <Button onClick={() => navigate(-1)} colorScheme="red">
-        취소
-      </Button>
-      <Button onClick={handleSubmit} colorScheme="orange">
-        수정
-      </Button>
-      </Stack>
-    </Container>
-  );
+  <Stack>
+    <Button onClick={() => navigate(-1)} colorScheme="red">
+      취소
+    </Button>
+    <Button onClick={handleSubmit} colorScheme="orange">
+      수정
+    </Button>
+  </Stack>
+</Container>
+
+)
+
 }
 
