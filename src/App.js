237f2e1863a55b3612deef24956d2b4a8cfb733e
Won A Young <zeronediary@naver.com>
2023-12-08 12:08:19 +0900

    App.js 수정 + npm install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-// import Category_CD from "./page/item/Category_CD";
+import Category_CD from "./page/item/Category_CD";
 // import { ItemAllPage } from "./component/ItemAllPage";
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from "react-router-dom";
-import {MemberSignup} from "./page/member/MemberSignup";
-import {HomeLayout} from "./page/layout/HomeLayout";
-import {MemberView} from "./page/member/MemberView";
-import {MemberEdit} from "./page/member/MemberEdit";
-import {MemberList} from "./page/member/MemberList";
-import {MemberLogin} from "./page/member/MemberLogin";
-import {LogInProvider} from "./component/LogInProvider";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { MemberSignup } from "./page/member/MemberSignup";
+import { HomeLayout } from "./page/layout/HomeLayout";
+import { MemberView } from "./page/member/MemberView";
+import { MemberEdit } from "./page/member/MemberEdit";
+import { MemberList } from "./page/member/MemberList";
 import { BoardList } from "./page/board/BoardList";
 import { BoardWrite } from "./page/board/BoardWrite";
-import { HomeLayout } from "./layout/HomeLayout";
+// import { HomeLayout } from "./layout/HomeLayout";
 import { BoardView } from "./page/board/BoardView";
 import { BoardEdit } from "./page/board/BoardEdit";
+import { OrderWrite } from "./page/order/OrderWrite";
+import Payment from "./page/payment/Payment";
+import { Success } from "./page/payment/Success";
+import { Fail } from "./page/payment/Fail";
+import { MemberLogin } from "./page/member/MemberLogin";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -30,6 +32,7 @@ const routes = createBrowserRouter(
       <Route path="edit/:id" element={<BoardEdit />}></Route>
       <Route path="category/:id" element={<Category_CD />} />
       <Route path="signup" element={<MemberSignup />}></Route>
+      <Route path="login" element={<MemberLogin />} />
       <Route path="member/list" element={<MemberList />} />
       <Route path="member" element={<MemberView />} />
       <Route path="medit/:id" element={<MemberEdit />} />
@@ -37,7 +40,6 @@ const routes = createBrowserRouter(
       <Route path="payment" element={<Payment />} />
       <Route path="success" element={<Success />} />
       <Route path="fail" element={<Fail />} />
-      {/*<Route path="login" element={<MemberLogin />} />*/}
     </Route>,
   ),
 );
