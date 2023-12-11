ab07c0058c22c1915404f1dec9a7041fd082453f
Won A Young <zeronediary@naver.com>
2023-12-11 12:29:32 +0900

    네브바 로그인 여부에 따라 다르게 + 로그아웃 기능

--- src/page/component/NavBar.js
+++ src/page/component/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Button, Flex, useToast } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,19 +14,75 @@ import {
 import axios from "axios";
 
 export function NavBar(props) {
+  const [loggedIn, setLoggedIn] = useState(false);
   const navigate = useNavigate();
   const urlParams = new URLSearchParams();
   const location = useLocation();
   const toast = useToast();
 
-  function handleLogout() {
+  function sendRefreshToken() {
+    const refreshToken = localStorage.getItem("refreshToken");
+    console.log("리프레시 토큰: ", refreshToken);
+
     axios
-      .post("/api/member/logout")
+      .get("/refreshToken", {
+        headers: { Authorization: `Bearer ${refreshToken}` },
+      })
+      .then((response) => {
+        console.log("sendRefreshToken()의 then 실행");
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+
+        console.log("토큰들 업데이트 리프레시 토큰: ");
+        console.log(response.data.refreshToken);
+        setLoggedIn(true);
+      })
+      .catch((error) => {
+        console.log("sendRefreshToken()의 catch 실행");
+        localStorage.removeItem("refreshToken");
+
+        setLoggedIn(false);
+      });
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken") !== null) {
+      console.log(localStorage.getItem("accessToken"));
+      axios
+        .get("/accessToken", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        .then(() => {
+          console.log("accessToken then 수행");
+          setLoggedIn(true);
+        })
+        .catch(() => {
+          sendRefreshToken();
+          localStorage.removeItem("accessToken");
+        })
+        .finally(() => console.log("finally loggedIn: ", loggedIn));
+    }
+    console.log("loggedIn: ", loggedIn);
+  }, [location]);
+
+  function handleLogout() {
+    console.log("handleLogout");
+    axios
+      .get("/api/logout", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
       .then(() => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
         toast({
           description: "성공적으로 로그아웃 되었습니다",
           status: "success",
         });
+        setLoggedIn(false);
         navigate("/");
       })
       .catch((error) => {
@@ -58,24 +114,28 @@ export function NavBar(props) {
       >
         앨범 등록
       </Button>
-      <Button
-        borderRadius={0}
-        variant="ghost"
-        size="lg"
-        leftIcon={<FontAwesomeIcon icon={faUserPlus} />}
-        onClick={() => navigate("/signup")}
-      >
-        회원가입
-      </Button>
-      <Button
-        borderRadius={0}
-        variant="ghost"
-        size="lg"
-        leftIcon={<FontAwesomeIcon icon={faUser} />}
-        onClick={() => navigate("/member?" + urlParams.toString())}
-      >
-        회원정보
-      </Button>
+      {loggedIn || (
+        <Button
+          borderRadius={0}
+          variant="ghost"
+          size="lg"
+          leftIcon={<FontAwesomeIcon icon={faUserPlus} />}
+          onClick={() => navigate("/signup")}
+        >
+          회원가입
+        </Button>
+      )}
+      {loggedIn && (
+        <Button
+          borderRadius={0}
+          variant="ghost"
+          size="lg"
+          leftIcon={<FontAwesomeIcon icon={faUser} />}
+          onClick={() => navigate("/member?" + urlParams.toString())}
+        >
+          회원정보
+        </Button>
+      )}
       <Button
         borderRadius={0}
         variant="ghost"
@@ -85,24 +145,28 @@ export function NavBar(props) {
       >
         회원목록
       </Button>
-      <Button
-        variant="ghost"
-        size="lg"
-        _hover={{ bg: "none" }}
-        onClick={() => navigate("/login")}
-        leftIcon={<FontAwesomeIcon icon={faRightToBracket} />}
-      >
-        로그인
-      </Button>
-      <Button
-        variant="ghost"
-        size="lg"
-        _hover={{ bg: "none" }}
-        onClick={handleLogout}
-        leftIcon={<FontAwesomeIcon icon={faRightFromBracket} />}
-      >
-        로그아웃
-      </Button>
+      {loggedIn || (
+        <Button
+          variant="ghost"
+          size="lg"
+          _hover={{ bg: "none" }}
+          onClick={() => navigate("/login")}
+          leftIcon={<FontAwesomeIcon icon={faRightToBracket} />}
+        >
+          로그인
+        </Button>
+      )}
+      {loggedIn && (
+        <Button
+          variant="ghost"
+          size="lg"
+          _hover={{ bg: "none" }}
+          onClick={handleLogout}
+          leftIcon={<FontAwesomeIcon icon={faRightFromBracket} />}
+        >
+          로그아웃
+        </Button>
+      )}
     </Flex>
   );
 }
