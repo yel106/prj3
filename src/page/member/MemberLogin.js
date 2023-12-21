8d4e7b8a008590240ae9a6c5db43c71a7b3f8de6
jeon602 <731aa@naver.com>
2023-12-21 20:01:08 +0900

    stack수정

--- src/page/member/MemberLogin.js
+++ src/page/member/MemberLogin.js
@@ -96,52 +96,52 @@ export function MemberLogin() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </FormControl>
-        <Button colorScheme="blue" onClick={handleLogin}>
-          로그인
-        </Button>
-        {/*카카오톡 로그인*/}
-        <VStack spacing={3}>
-          <Button
-            style={{
-              backgroundImage: `url('${imagePrefix}kakao_login_medium_narrow.png')`,
-              width: "183px",
-              height: "45px",
-            }}
-            onClick={() => handleSocialLogin("KAKAO")}
-          />
-          {/*네이버로그인*/}
-          <Button
-            style={{
-              backgroundImage: `url('${imagePrefix}btnG_%EC%99%84%EC%84%B1%ED%98%95.png')`,
-              width: "183px",
-              height: "45px",
-              backgroundSize: "183px 45px",
-            }}
-            onClick={() => handleSocialLogin("NAVER")}
-          />
-          {/*구글 로그인*/}
-          <Button
-            backgroundColor="#FFF"
-            boxShadow="md"
-            w="183px"
-            h="45px"
-            _hover="none"
-            leftIcon={
-              <Img
-                objectFit="cover"
-                boxSize="20px"
-                mr={2}
-                src={`${imagePrefix}%EA%B5%AC%EA%B8%80+%EB%A1%9C%EA%B3%A0.png`}
-              />
-            }
-            onClick={() => handleSocialLogin("GOOGLE")}
-            fontFamily="Roboto"
-            fontWeight="500"
-          >
-            Google
-          </Button>
-        </VStack>
       </HStack>
+      <Button colorScheme="blue" onClick={handleLogin}>
+        로그인
+      </Button>
+      {/*카카오톡 로그인*/}
+      <VStack spacing={3}>
+        <Button
+          style={{
+            backgroundImage: `url('${imagePrefix}kakao_login_medium_narrow.png')`,
+            width: "183px",
+            height: "45px",
+          }}
+          onClick={() => handleSocialLogin("KAKAO")}
+        />
+        {/*네이버로그인*/}
+        <Button
+          style={{
+            backgroundImage: `url('${imagePrefix}btnG_%EC%99%84%EC%84%B1%ED%98%95.png')`,
+            width: "183px",
+            height: "45px",
+            backgroundSize: "183px 45px",
+          }}
+          onClick={() => handleSocialLogin("NAVER")}
+        />
+        {/*구글 로그인*/}
+        <Button
+          backgroundColor="#FFF"
+          boxShadow="md"
+          w="183px"
+          h="45px"
+          _hover="none"
+          leftIcon={
+            <Img
+              objectFit="cover"
+              boxSize="20px"
+              mr={2}
+              src={`${imagePrefix}%EA%B5%AC%EA%B8%80+%EB%A1%9C%EA%B3%A0.png`}
+            />
+          }
+          onClick={() => handleSocialLogin("GOOGLE")}
+          fontFamily="Roboto"
+          fontWeight="500"
+        >
+          Google
+        </Button>
+      </VStack>
     </Box>
   );
 }
