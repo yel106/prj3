15a554c9b92ef54669b07dd9001930eca1f1b25e
yel106 <yel_@hanmail.net>
2023-12-28 19:42:59 +0900

    url 수정함
    axios
          .putForm(
            "/api/board/edit/" + id,

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -122,7 +122,7 @@ export function BoardEdit() {
 
     axios
       .putForm(
-        "https://api.muemalaf.click/api/board/edit/" + id,
+        "/api/board/edit/" + id,
         {
           title: board.title,
           artist: board.artist,
